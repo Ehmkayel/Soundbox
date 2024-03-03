f04cccc354c0bcb71aa28adb3235939b6c5ff7b7
lamidimorufat <lamidimorufat0@gmail.com>
2024-03-03 18:48:26 +0100

    worked on the popup for the bottom buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const menu = document.querySelector("#menu");
 const openNav = document.querySelector("#open-nav");
 const closeNav = document.querySelector("#close-nav");
 const search = document.querySelector(".search");
+const buttons = document.querySelectorAll(".popup");
 
 openNav.addEventListener("click", () => {
     menu.style.display = "flex";
@@ -16,8 +17,20 @@ closeNav.addEventListener("click", () =>{
     search.style.display = "none";
 })
 
-function largeImage(){
-    let image = document.createElement("img")
-    image.src = "assets/mainheadset.png";
-    document.body.appendChild(image)
-}
+buttons.forEach(button => {
+    const popup = button.closest('.item').querySelector('.wrapper');
+    const close = button.closest('.item').querySelector('.container-close');
+
+button.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+
+
+  popup.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+});
